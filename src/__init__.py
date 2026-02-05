bce0e38dbea47925b6354500d86c73f022247a98
Indrajeet Aditya Roy <roy.indrajeet009@gmail.com>
2026-02-05 17:55:22 -0500

    refactor: reorganize flat src/ modules into domain subpackages

--- src/__init__.py
+++ src/__init__.py
@@ -4,5 +4,3 @@ Locality Curse Research Framework
 A research framework for studying inductive bias mismatch in heterogeneous
 knowledge distillation, specifically CNN-to-ViT transfer.
 """
-
-__version__ = "1.0.0"
